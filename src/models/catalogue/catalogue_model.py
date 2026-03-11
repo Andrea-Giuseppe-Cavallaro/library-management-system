5d07899b45fe6133e216ab781be6d2a9909a17cb
Andrea Giuseppe Cavallaro <adenor88@gmail.com>
2026-03-11 23:49:18 +0100

    feat: enhance models structure with new methods and relationships; add reader service for external book data retrieval

--- src/models/catalogue/catalogue_model.py
+++ src/models/catalogue/catalogue_model.py
@@ -1,1 +1,21 @@
-# pylint: skip-file
+from src.models.base import SerializerMixin, db
+
+
+class CatalogueModel(db.Model, SerializerMixin):
+    __tablename__ = "catalogues"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, default="Main Catalogue")
+
+    books = db.relationship("BookModel", back_populates="catalogue", lazy="select")
+
+    def update_from_dict(self, data: dict) -> None:
+        self.name = data["name"]
+        self.books = data["books"]
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "books": self._get_ids_from_relation("books"),
+        }
