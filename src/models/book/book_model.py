ab2f0efd0f08723d3b75898e7fbc57c09eec468d
elfroot-sj <eleonora.arcuri@stevejobs.academy>
2026-03-11 11:41:26 +0100

    feat: add sqlalchemy models

--- src/models/book/book_model.py
+++ src/models/book/book_model.py
@@ -1,1 +1,20 @@
-# pylint: skip-file
+from ..base import db
+
+
+class BookModel(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    authors = db.Column(db.String(500), nullable=False)
+    languages = db.Column(db.String(255), nullable=False)
+    first_publish_year = db.Column(db.Integer, nullable=True)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "title": self.title,
+            "authors": self.authors.split(","),
+            "languages": self.languages.split(","),
+            "first_publish_year": self.first_publish_year,
+        }
